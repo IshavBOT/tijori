f311bb29375bb5f9c9044c543c6b5a85aef69f50
IshavBOT <notishav@gmail.com>
2024-08-12 22:20:37 +0530

    initial comm its

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Adding Songs Man")
+
+async function main(){
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+}
+
+main()
